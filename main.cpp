dcca9ca15279670aca0b5166d7d5151b0e9820d2
JaehongParkYS <woghd8754@gmail.com>
2020-11-09 23:48:39 +0900

    Jenkins webhook test

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@ TEST_F(CalculatorTest, plus_p)
 
 int main(int argc, char **argv)
 {
+    std::cout << "Hello Jenkins" << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
